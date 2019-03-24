71149fff1cfce9d781ba9cdc001c0749d7290254
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2019-03-24 05:31:23 -0700

    Update WOLF.py

--- WOLF.py
+++ WOLF.py
@@ -1,13 +1,18 @@
-import os
+import os,time
 
-os.system("apt update")
-print ''' 
+print('157375')
+time.sleep(2)
+print('244274')
+time.sleep(3)
+print('738481')
+time.sleep(4)
+print("please Wait ")
+time.sleep(5)
+print("                          ")
+print("                         ")
+print('                              ')
+print("goooood  hacker account facebook Gooood")
+print('                                     ')
 
-
-------good--HAcker-facebook- Account--
-
-
-
-'''
 print("\033[0;98m--- Good \033[0;97m---- Link      \033[0;96m   https://justpaste.it/516kv"     )
 
