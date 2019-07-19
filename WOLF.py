f5f2f2590d896044957fd396136cbffc98390286
yehia hacker <42983220+yehia-hacker@users.noreply.github.com>
2019-07-19 07:49:53 -0700

    Update WOLF.py

--- WOLF.py
+++ WOLF.py
@@ -1,18 +1,2 @@
-import os,time
-
-print('157375')
-time.sleep(2)
-print('244274')
-time.sleep(3)
-print('738481')
-time.sleep(4)
-print("please Wait ")
-time.sleep(5)
-print("                          ")
-print("                         ")
-print('                              ')
-print("goooood  hacker account facebook Gooood")
-print('                                     ')
-
-print("\033[0;98m--- Good \033[0;97m---- Link      \033[0;96m   https://pastebin.com/Mtc4MYiJ"     )
-
+#Stop Tool
+print "https://www.facebook.com/yehia.hacker"
